5424157a44d15fe6aa4c21ab810e69ff9ce40f52
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-08-28 22:55:18 +0300

    Add UbRecite function to emit festive messages

---
+++ UB_DAY.js
@@ -0,0 +1,28 @@
+function UbRecite(){
+  socket.emit("We wish you a very Ub Day,");
+  setTimeout(()=>{
+    socket.emit("We wish you a merry Christmas,");
+    setTimeout(()=>{
+      socket.emit("We wish you a very Ub-mas,");
+      setTimeout(()=>{
+        socket.emit("And a happy new year!");
+        setTimeout(()=>{
+          socket.emit("Good bonzings we bring to you and your Black Santa Claupse,");
+          setTimeout(()=>{
+            socket.emit("We wish you a verity, and a 7 b bot commands,");
+            setTimeout(()=>{
+              socket.emit("Now bring us some bitcoin, Now bring us some big ol YTP");
+              setTimeout(()=>{
+                socket.emit("Now bring us some Figgy pudding, And bring some out here");
+                setTimeout(()=>{
+                  socket.emit("Merry 2018 to 2026 collaboration");
+                },3000)
+              },3000)
+            },3000)
+          },3000)
+        },3000)
+      },1000)
+    },1000)
+  },1000)
+}
+UbRecite()
